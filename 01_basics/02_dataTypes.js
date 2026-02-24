c7f3952af04e0784341396cb7f3b0fcd990e185d
Priyusalve <140909805+Priyusalve@users.noreply.github.com>
2026-02-24 18:07:43 +0000

    change in datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) //we are using node JS not browser
+
+console.log(3+
+    3) // code readability should be high
+
+console.log("Priyanka")
+
+// original documentation of JS is in tc39.es use MDN doc it is easy
+
+let name = "Priyanka"
+let age = 30
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => 
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(Typeof null) //Object
