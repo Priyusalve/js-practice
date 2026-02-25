52b12dc1264a52574421f6384d52c3e8af494777
Priyusalve <140909805+Priyusalve@users.noreply.github.com>
2026-02-25 17:04:47 +0000

    datatypes

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id == anotherId);
+
+const bigNumber = 376835742362638729n
+console.log(typeof bigNumber);
+
+
+// Reference type (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["superman", "hanuman"] //array in square bracket
+
+let byObj = {        // Objects in curly braces
+    name: "Priyanka",
+    age: 30,
+}
+
+// console.log(byObj);
+
+
