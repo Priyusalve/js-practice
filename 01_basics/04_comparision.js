521cea0392a87e8df5e108de538e0c7eb3c1d596
Priyusalve <140909805+Priyusalve@users.noreply.github.com>
2026-02-25 16:46:13 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2!=2);
+
+// console.log("02" > 1); // true which is not correct the datatype should be same for comparision
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // true unable to compare
+
+// console.log(undefined >= 0); // false
+
+// === this is strick check
+
+console.log("2" === 2);
+
